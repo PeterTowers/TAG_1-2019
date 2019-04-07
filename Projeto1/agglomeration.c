941c2b4c9a08e6b2f2ce3b31637a1c6ebda9d5f3
ghalestrilo <ghalestrilo@gmail.com>
2019-04-07 20:22:21 -0300

    Split bron-kerbosch methods

--- Projeto1/agglomeration.c
+++ Projeto1/agglomeration.c
@@ -1,7 +1,25 @@
 #include "network.h"
 
-float agglomeration(VERTEX v){
-  return 1.0;
+#include "bklist.h"
+
+int length(BK_LIST* list){
+  return list == NULL
+    ? 0
+    : 1 + length(list->next);
+}
+
+int triangle_count(VERTEX pivot, BK_LIST* neighbors){
+  
+};
+
+float agglomeration(NETWORK* network, int index){
+  VERTEX pivot = network->vertex[index];
+  BK_LIST* neighbors = find_neighbours(network, NULL, pivot, 0);
+
+  float t = (float) triangle_count(pivot, neighbors);
+
+  destroy(neighbors);
+  return (2.0 * t) / (pivot.degree * (pivot.degree - 1));
 }
 
 float avg_agglomeration(NETWORK* network){
@@ -9,7 +27,7 @@ float avg_agglomeration(NETWORK* network){
   float acc = 0.0;
 
   for(int i = 0; i < network->nvertices; i++)
-    acc += agglomeration(network->vertex[i]);
+    acc += agglomeration(network, i);
 
   return (acc / network->nvertices);
 }
