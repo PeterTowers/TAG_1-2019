1e92cc9445d07fc45c1682a41a125bee5528c24b
ghalestrilo <ghales.trilo@gmail.com>
2019-04-08 20:00:39 -0300

    Cleanup and handoff preparation

--- Projeto1/agglomeration.c
+++ Projeto1/agglomeration.c
@@ -2,37 +2,55 @@
 
 #include "bklist.h"
 
-int length(BK_LIST* list){
-  return (list == NULL)
-    ? 0
-    : 1 + length(list->next);
-}
-
+// Função que conta todos os triangulos existentes a partir de um no, percorrendo dois vizinhos interligados
+// Esse calculo torna-se a base do calculo do coeficiente de aglomeracao
 int triangle_count(VERTEX pivot, NETWORK* network){
   int result = 0;
   if (network == NULL) return 0; // CLEANUP: probably unnecessary
 
-  BK_LIST *neighbors = find_neighbours(network, NULL, pivot, 0);
+  // Encontramos os vizinhos (i) do no original (pivot)
+  BK_LIST *n = find_neighbours(network, NULL, pivot, 0);
   
-  if (length(neighbors) < 2) return -1;
+  // Caso um no tenha menos de dois vizinhos, nao ha como calcular o coeficiente
+  if (length(n) < 2) return -1;
   
-  for(BK_LIST *i = neighbors, *head = NULL; i != NULL; i = i->next){
-    head = find_neighbours(network, NULL, i->vertex, 0);
+  // Percorrimento da lista de vizinhos como segundos vertices do triangulo
+  for(BK_LIST *i = n, *nn = NULL; i != NULL; i = i->next){
 
-    for(BK_LIST *j = head; j != NULL; j = j->next)
-      if (j->vertex.id == pivot.id)
-          result++;
+    // Encontramos os vizinhos de segundo grau (j) de cada vizinho (i)
+    nn = find_neighbours(network, NULL, i->vertex, 0);
 
-    destroy(head);
+    // Percorrimento dos vizinhos de segundo grau como terceiros vertices
+    for(BK_LIST *j = nn, *nnn = NULL; j != NULL; j = j->next){
+
+      // Encontramows os vizinhos de terceiro grau (k) de cada vizinho de segundo grau (j)
+      nnn = find_neighbours(network, NULL, j->vertex, 0);
+
+      // Caso o no original (pivot) seja vizinho dos seus vizinhos de segundo grau
+      // Ou seja, caso o pivot seja vizinho de terceiro grau dele proprio
+      // Incrementamos o contador, contando o triangulo recem formado
+      for(BK_LIST *k = nnn; k != NULL; k = k->next)
+        if (k->vertex.id == pivot.id){
+            #ifdef DEBUG_PRINT_TRIANGLES
+            printf("\ttriangulo: %d -> %d -> %d -> %d\n", pivot.id, i->vertex.id, j->vertex.id, k->vertex.id);
+            #endif
+
+            result++;
+        }
+      destroy(nnn);
+    }
+
+    destroy(nn);
   }
 
-  destroy(neighbors);
+  destroy(n);
+
+  // Resultado e dividido por dois, pois cada triangulo foi contado duas vezes
   return result / 2;
-  /** Since every triangle is counted twice, we divide the result by 2
-   *  This avoids an otherwise unnecessary and horrendous allocation nightmare to check each triangle
-   */
 };
 
+
+// Calcula a aglomeracao em um dado triangulo, indicado pelo proprio indice na lista de nos da rede
 float agglomeration(NETWORK* network, int index){
   VERTEX pivot = network->vertex[index];
   float t = (float) triangle_count(pivot, network);
@@ -43,12 +61,13 @@ float agglomeration(NETWORK* network, int index){
     : ((2.0 * t) / (float) (pivot.degree * (pivot.degree - 1)));
 }
 
+// Calcula a aglomeracao media em um grafo, somando individualmente as aglomeracoes de cada no
+// E dividindo pela contagem total de nos
 float avg_agglomeration(NETWORK* network){
   float acc = 0.0;
 
   for(int i = 0; i < network->nvertices; i++)
     acc += agglomeration(network, i);
 
-  printf("acc: %f\n", acc);
   return acc / ((float) network->nvertices);
 }
