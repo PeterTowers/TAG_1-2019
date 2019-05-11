99ac78360ea7e928f0c02f4b79ab2a1eae6cfacb
Pedro Torres <pedrohagatorres@gmail.com>
2019-05-11 15:25:58 -0300

    Added documentation to class 'course'.

--- Projeto2/include/course.hpp
+++ Projeto2/include/course.hpp
@@ -3,13 +3,15 @@
 #include <vector>
 #include <functional>
 
+// The course class saves data parsed from file '../data/materias.txt'
 class course
 {
 public:
-    const unsigned int id;
-    const unsigned int credits;
-    const std::string name;
+    const unsigned int id;      // Course's identification code ("Codigo")
+    const unsigned int credits; // Course's credits ("Creditos")
+    const std::string name;     // Course's name ("Nome da disciplina")
 
+    // Class' constructor and destructor
     course(unsigned int, std::string, unsigned int, std::function<void(course)> callback = [](course){});
     ~course();
 };
