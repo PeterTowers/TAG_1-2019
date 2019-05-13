27814664cf675d4fc7125ba0b2899b82d8c0e42c
lalis <ghalestrilo@gmail.com>
2019-05-12 23:11:43 -0300

    Fix main.cpp documentation and includes

--- Projeto2/include/reader.hpp
+++ Projeto2/include/reader.hpp
@@ -1,5 +1,4 @@
 #pragma once
-#include "toml.h"
 #include "graph.hpp"
 #include "course.hpp"
 
--- Projeto2/src/main.cpp
+++ Projeto2/src/main.cpp
@@ -27,21 +27,10 @@
 #include <iostream>
 #include "../include/reader.hpp"
 #include "../include/graph.hpp"
-#include "../include/topoSorting.hpp"
 
 int main(int argc, char const *argv[])
 {
     auto print_course = [](course c){ std::cout << c.name; };
-
-    // std::vector<std::string> args;
-
-    // for(int i = 1; i < argc; i++) // skip program name
-    //     args.push_back(argv[i]);
-
-
-    // // Supondo filename como arg[0]
-    // auto graph = read(args[0]);
-    // if (graph == nullptr) return -1;
 
     // Builds the digraph onto object 'graph'
     digraph<course>* graph = build("../data/materias.txt");
