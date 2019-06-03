2dc5519abfe05145b2f9689d36aea727a74042c9
Pedro Torres <pedrohagatorres@gmail.com>
2019-06-03 11:52:20 -0300

    Added print to '.dot' file functionality to 'printer' (not implemented).

--- Projeto3/include/printer.hpp
+++ Projeto3/include/printer.hpp
@@ -1,9 +1,13 @@
 #pragma once
 
 #include <iostream>
+#include <fstream>
 #include <vector>
 
-#include "Edge.hpp"
+#include "../include/Edge.hpp"
+#include "../include/Node.hpp"
+#include "../include/School.hpp"
+#include "../include/Teacher.hpp"
 
 /** PRINTER MODULE
  * This module deals with printing a graph.
@@ -13,3 +17,5 @@
  * Receives a vector of paired edges and prints its content to the console.
  */
 void print_to_console(std::vector<Edge> edges);
+
+void print_adj(std::vector<School> schools, std::vector<Teacher> teachers);
--- Projeto3/src/printer.cpp
+++ Projeto3/src/printer.cpp
@@ -24,3 +24,65 @@ void print_to_console(std::vector<Edge> edges) {
             std::cout << " \t";
     }
 }
+
+// Function print_adj() prints adjacency list
+template<class T>
+void print_adj(std::vector<School> schools, std::vector<Teacher> teachers) {
+
+    // Variable for file handling
+    std::ofstream outputFile;
+
+    // Open file in WRITE mode, passing 'trunc' parameter so that if there's a previous file, it'll be ERASED
+    outputFile.open("../data/adjacency_list.dot", std::ios::trunc);
+
+    // Prints an error if the file wasn't opened correctly and exits program with code '-3'
+    if (!outputFile) {
+        std::cout << "Error: Could not open file for adjacency list." << std::endl;
+        exit(-3);
+    }
+
+    auto find_element_by_id = [](std::vector<Node> elements, int id) {
+        for (int i = 0; i < elements.size(); i++)
+            if (elements[i].get_id() == id)
+                return i;
+
+        return -1;
+    };
+
+    // Prints header as a comment
+    outputFile << "/* ----- Grafo relacionando professores e escolas desejadas ----- */\n";
+
+    // Print the type of graph (graph) followed by a title (label) to be displayed within the image
+    outputFile << "graph {\n";
+    outputFile << "\tlabel=\"Grafo relacionando professores e escolas desejadas\";\n";
+
+    // Builds graph with left-to-right orientation (no need for this here)
+    //outputFile << "\trankdir=LR;\n";
+
+    // Prints each node's id in 'nodes'...
+    for (auto& teacher : teachers) {
+        for (int i = 0; i < teacher.get_schools().size(); i++) {
+            // ... with an indentation
+            outputFile << "\tP" << teacher.get_id() << " -- E";
+
+            int schoolIndex = find_element_by_id(schools, teacher.get_schools()[i]);
+
+            if (schoolIndex != -1) {
+                outputFile << schools[schoolIndex];
+                outputFile << "[label=\"" << i+1 << "\",weight=\"" << i+1 << "\"];\n";
+            }
+
+            else {
+                std::cout << "Error: could not find school id." << std::endl;
+                exit(-4);
+            }
+        }
+
+    }
+
+    // Prints '}' signifying the graph's end
+    outputFile << '}';
+
+    // Closes the file
+    outputFile.close();
+}
