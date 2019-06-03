e045f35a840c1feb0d49710083898b45e15d9825
Pedro Torres <pedrohagatorres@gmail.com>
2019-06-03 00:34:35 -0300

    Added 'print_to_console()' function.

---
+++ Projeto3/include/printer.hpp
@@ -0,0 +1,15 @@
+#pragma once
+
+#include <iostream>
+#include <vector>
+
+#include "Edge.hpp"
+
+/** PRINTER MODULE
+ * This module deals with printing a graph.
+ */
+
+/** print_to_console
+ * Receives a vector of paired edges and prints its content to the console.
+ */
+void print_to_console(std::vector<Edge> edges);
---
+++ Projeto3/src/printer.cpp
@@ -0,0 +1,24 @@
+#include "../include/printer.hpp"
+
+// print_to_console prints the content of a vector of Edges to the console
+void print_to_console(std::vector<Edge> edges) {
+    // Prints message
+    std::cout << "Impressao do pareamento.\nUma escola ou vaga associada ao elemento 'P0' indica ";
+    std::cout << "que ela nao foi pareada a nenhum professor.\n" << std::endl;
+
+    int i = 0;  // Auxiliary variable to count printed edges
+
+    // Iterates through vector of edges
+    for (auto& edge : edges) {
+        std::cout << "Escola: E" << edge.from() << " -> ";  // School related
+        std::cout << "Professor: P" << edge.to();           // Teacher related
+
+        i++;    // Counts printed edges
+
+        // End line every 2 pair of edges printed or insert a horizontal tab
+        if ( (i%2) == 0 )
+            std::cout << std::endl;
+        else
+            std::cout << " \t";
+    }
+}
