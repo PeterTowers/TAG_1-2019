2ce9416b903a4945e5984093d612447ed0dbb7ef
ghalestrilo <ghales.trilo@gmail.com>
2019-07-01 14:44:08 -0300

    :art: Add header to main.cpp

--- Projeto4/src/main.cpp
+++ Projeto4/src/main.cpp
@@ -1,16 +1,28 @@
 #include <iostream>
 #include "../include/UserInterface.hpp"
 
+/* Universidade de Brasília
+ *
+ * Disciplina: Teoria e Aplicação de Grafos - Turma A, 01/2019
+ *
+ * Projeto 3 - Problema de pareamento
+ *
+ * Este programa utiliza o arquivo './data/data.txt' para montar um grafo não direcionado e sem peso. Sobre esse grafo,
+ * implementamos o cálculo de pareamento utilizando o algoritmo descrito no artigo "Two Algorithms for the
+ * Student-Project Allocation Problem" de  ABRAHAM, D. J.; IRVING, R. W.; MANLOVE, D. F.
+ *
+ * Por uma questão de tempo, não foi possível concluir todos os requisitos desse projeto, portanto, o que é entregue
+ * aqui é um programa que, de acordo com o artigo supracitado, gera um pareamento máximo e estável, favorável aos
+ * professores.
+ *
+ * Autores:
+ * Pedro Lucas Silva Haga Torres - 16/0141575
+ * Thales Gonçalves Grilo        - 14/0163603
+ *
+ * Código criado em 01/07/2019, disponível em: https://github.com/PeterTowers/TAG_1-2019/tree/master/Projeto3
+*/
+
 int main(){
     user_interface();
-
-//  Sudoku s;
-//
-//  system("clear");
-//
-//  //s.solve(true);
-//
-//  s.generate();
-
   return 0;
 }
